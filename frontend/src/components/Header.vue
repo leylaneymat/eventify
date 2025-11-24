7ebd7f2d7256c6824beadebec69058c945ad09b0
Ilqar Malikov <melikovi908@gmail.com>
2025-11-24 18:51:56 +0400

    Adding header layout with basic structure.

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <h1 class="app-name">Eventify</h1>
+    </div>
+
+    <div class="header-right">
+      <el-button type="text" @click="openLoginModal">Login</el-button>
+      <el-button type="text" @click="openRegisterModal">Register</el-button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const loginDialogVisible = ref(false)
+const registerDialogVisible = ref(false)
+
+const openLoginModal = () => loginDialogVisible.value = true
+const openRegisterModal = () => registerDialogVisible.value = true
+</script>
+
+<style scoped>
+.header {
+  background-color: #409eff;
+  color: white;
+  padding: 16px;
+  display: flex;
+  justify-content: space-between;
+}
+</style>
