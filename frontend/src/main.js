131553cf3a8ec923940fdceb48d8a47690dcf001
fbakirli <113505730+fbakirli@users.noreply.github.com>
2025-11-24 11:03:45 +0400

    setup userStore and elementPlus

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
-import './style.css'
+import { createPinia } from 'pinia'
+import ElementPlus from 'element-plus'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
+
+app.use(createPinia())
+app.use(ElementPlus)
+
+app.mount('#app')
