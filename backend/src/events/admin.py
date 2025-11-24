60bece67eb415a290c65b4b254c5544a0c914d47
ELVINHAYATOV <ehayatov17606@ada.edu.az>
2025-11-24 16:25:24 +0400

    Setup permissions

--- backend/src/events/admin.py
+++ backend/src/events/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
-# Register your models here.
+from events.models import Comment, Event, Like, Ticket
+
+
+class TicketInline(admin.StackedInline):
+    model = Ticket
+    extra = 1
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    inlines = [TicketInline]
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    pass
