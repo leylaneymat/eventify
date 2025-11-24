2f1ce70d2bd617d0fc07d8c5f2437071e4871c0f
ELVINHAYATOV <ehayatov17606@ada.edu.az>
2025-11-24 13:54:13 +0400

    Create models
    Creates events for model, ticket, Like and Comment

--- backend/src/events/models.py
+++ backend/src/events/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Ticket(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)  # 99,999,999.99
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="tickets")
+
+    def __str__(self):
+        return self.name
+
+
+class Like(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="likes")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="likes")
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=["event", "user"], name="unique_like"),
+        ]
+
+    def __str__(self):
+        return f'Event "{self.event}", User "{self.user}"'
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    event = models.ForeignKey(Event, on_delete=models.CASCADE, related_name="comments")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
+
+    def __str__(self):
+        return self.text
