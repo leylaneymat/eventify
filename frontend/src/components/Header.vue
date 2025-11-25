5176f29158b8c5f4d6cb62acfb58237c0b893047
Ilqar Malikov <melikovi908@gmail.com>
2025-11-25 17:48:23 +0400

    Add login and register dialogs

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -9,6 +9,30 @@
       <el-button type="text" @click="openRegisterModal">Register</el-button>
     </div>
   </div>
+  
+  <el-dialog v-model="loginDialogVisible" title="Login" width="500px">
+  <el-form :model="loginForm">
+    <el-form-item label="Username">
+      <el-input v-model="loginForm.username" />
+    </el-form-item>
+    <el-form-item label="Password">
+      <el-input v-model="loginForm.password" type="password" />
+    </el-form-item>
+    <el-button type="primary">Login</el-button>
+  </el-form>
+</el-dialog>
+
+<el-dialog v-model="registerDialogVisible" title="Register" width="500px">
+  <el-form :model="registerForm">
+    <el-form-item label="Username">
+      <el-input v-model="registerForm.username" />
+    </el-form-item>
+    <el-form-item label="Password">
+      <el-input v-model="registerForm.password" type="password" />
+    </el-form-item>
+  </el-form>
+</el-dialog>
+
 </template>
 
 <script setup>
