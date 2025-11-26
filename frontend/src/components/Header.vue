e82bd795954179c7d865d681c2967cfcd7c18e70
Ilqar Malikov <melikovi908@gmail.com>
2025-11-26 14:43:35 +0400

    Adding purchasedTickets dialog.

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -5,11 +5,44 @@
     </div>
 
     <div class="header-right">
-      <el-button type="text" @click="openLoginModal">Login</el-button>
-      <el-button type="text" @click="openRegisterModal">Register</el-button>
+      <template v-if="userStore.isLoggedIn">
+        <el-button type="text" @click="showPurchased">Purchased tickets</el-button>
+        |
+        <span>{{ userStore.user?.username }}</span>
+        <el-button type="text" @click="userStore.logout">Logout</el-button>
+      </template>
+      <template v-else>
+        <el-button type="text" @click="openLoginModal">Login</el-button>
+        <el-button type="text" @click="openRegisterModal">Register</el-button>
+      </template>
     </div>
-  </div>
   
+  <el-dialog v-model="purchasedTicketsDialogVisible" title="Purchased Tickets" width="700px">
+      <div v-if="loading" class="loading-container">
+        <el-icon class="is-loading">
+          <Loading />
+        </el-icon>
+        <p>Loading tickets...</p>
+      </div>
+      <div v-else-if="purchasedTickets.length === 0" class="no-tickets">
+        <p>You haven't purchased any tickets yet.</p>
+      </div>
+      <el-table v-else :data="purchasedTickets" style="width: 100%">
+        <el-table-column label="Event Name" prop="event.name" width="200" />
+        <el-table-column label="Ticket Name" prop="ticket.name" width="150" />
+        <el-table-column label="Price" width="100">
+          <template #default="scope">
+            ${{ scope.row.ticket.price }}
+          </template>
+        </el-table-column>
+        <el-table-column label="Purchase Date" pro width="200">
+          <template #default="scope">
+            {{ scope.row.purchaseDate }}
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-dialog>
+
   <el-dialog v-model="loginDialogVisible" title="Login" width="500px">
   <el-form :model="loginForm">
     <el-form-item label="Username">
@@ -33,6 +66,7 @@
   </el-form>
 </el-dialog>
 
+</div>
 </template>
 
 <script setup>
@@ -56,6 +90,18 @@ const submitLogin = async () => {
   } else {
     ElMessage.error('Login failed')
   }
+}
+
+const purchasedTicketsDialogVisible = ref(false)
+const purchasedTickets = ref([])
+const loading = ref(false)
+
+const showPurchased = async () => {
+  purchasedTicketsDialogVisible.value = true
+  loading.value = true
+  const result = await axios.get(`http://localhost:8000/api/v1/users/${userStore.user.id}/purchased_tickets/`)
+  purchasedTickets.value = result.data
+  loading.value = false
 }
 
 </script>
