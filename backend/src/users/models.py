6cf60e0d2d0454001290ee8b3cecdfbc11a657e3
leylaneymat <123471054+leylaneymat@users.noreply.github.com>
2025-11-26 21:09:39 +0400

    Add PurchasedTicket model

--- backend/src/users/models.py
+++ backend/src/users/models.py
@@ -4,3 +4,21 @@ from django.db import models
 
 class User(AbstractUser):
     REQUIRED_FIELDS = []
+
+
+class PurchasedTicket(models.Model):
+    event = models.ForeignKey(
+        "events.Event",
+        on_delete=models.CASCADE,
+        related_name="purchased_tickets_by_users",
+    )
+    ticket = models.ForeignKey(
+        "events.Ticket", on_delete=models.CASCADE, related_name="purchased_by_users"
+    )
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="purchased_tickets"
+    )
+    purchase_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Ticket "{self.ticket}" for Event "{self.event}", User "{self.user}"'
