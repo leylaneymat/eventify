a805edf7ffda149f655183e7ce803b837667abdb
leylaneymat <123471054+leylaneymat@users.noreply.github.com>
2025-11-26 21:45:21 +0400

    Setup Permissions

--- backend/src/users/admin.py
+++ backend/src/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import PurchasedTicket, User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    pass
+
+
+@admin.register(PurchasedTicket)
+class PurchasedTicketAdmin(admin.ModelAdmin):
+    pass
