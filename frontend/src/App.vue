60b0ea122bf04d8026679f8cd47c2c08549412fa
Ilqar Malikov <melikovi908@gmail.com>
2025-11-27 20:47:46 +0400

    Adding Footer.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,8 +11,9 @@
 <script>
 import Header from '@/components/Header.vue'
 import EventCard from '@/components/EventCard.vue'
+import Footer from '@/components/Footer.vue'
 export default {
-  components: { Header, EventCard },
+  components: { Header, EventCard, Footer },
   data() {
     return { events: [] }
   }
---
+++ frontend/src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer class="w-full py-6 border-t border-gray-200 bg-white dark:bg-gray-900">
+      <div class="mx-auto max-w-7xl px-4 text-center text-sm text-gray-600 dark:text-gray-300">
+        <span>Made with</span>
+        <span class="mx-1">❤️</span>
+        <a
+          href="https://github.com/leylaneymat"
+          target="_blank"
+          class="font-medium hover:underline text-blue-600 dark:text-blue-400"
+        >
+          Team 10
+        </a>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer'
+}
+</script>
