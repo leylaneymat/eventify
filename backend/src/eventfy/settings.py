d390a7dfae980a9744702ded3e7ca0dfdb7faca5
leylaneymat <123471054+leylaneymat@users.noreply.github.com>
2025-12-13 15:17:52 +0400

    fixes backend issue with sending email

--- backend/src/eventfy/settings.py
+++ backend/src/eventfy/settings.py
@@ -89,6 +89,21 @@ DATABASES = {
     }
 }
 
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
